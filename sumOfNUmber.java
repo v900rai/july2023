5a0f15260d32d79c7e5a4b55511cb713d1b89aa1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-07-02 19:27:51 +0530

    Add files via upload

---
+++ sumOfNUmber.java
@@ -0,0 +1,13 @@
+package target200qQQQTTOOnnSS;
+
+public class sumOfNUmber 
+{
+	public static void main(String []args)
+	{
+		int number=10;
+		int numberr=20;
+		int result=number+numberr;
+		System.out.println(result);
+	}
+
+}

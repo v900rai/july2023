faf5fff1580458192552fa7b735e8b38cbf926af
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-07-02 19:26:09 +0530

    Add files via upload

---
+++ ReverseNumberExample.java
@@ -0,0 +1,17 @@
+package target200qQQQTTOOnnSS;
+
+public class ReverseNumberExample
+{
+	public static void main(String []args)
+	{
+		int number = 12345678, reverse = 0;  
+		while(number != 0)   
+		{  
+		int remainder = number % 10;  
+		reverse = reverse * 10 + remainder;  
+		number = number/10;  
+		}  
+		System.out.println("The reverse of the given number is: " + reverse);  
+		}  
+
+}

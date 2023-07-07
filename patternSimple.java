5ad65417f23b2981ce39ca66ed4963102be72cda
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-07-07 21:57:45 +0530

    Add files via upload

---
+++ patternSimple.java
@@ -0,0 +1,17 @@
+package selfpratices;
+
+public class patternSimple 
+{
+	public static void main(String[] args)
+	{
+		for(int i=6; i>=0; i--)
+		{
+			for(int j=i; j>0; j--)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
